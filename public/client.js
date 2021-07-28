77bade155fa6da2ec6e165a5461bcd693fd4d3ed
Kingston <liamyoung@ufl.edu>
2021-07-28 11:00:34 -0400

    New changes, leaving to Chicago

--- public/client.js
+++ public/client.js
@@ -1,9 +1,12 @@
 import * as THREE from 'https://threejsfundamentals.org/threejs/resources/threejs/r127/build/three.module.js'
 import { GLTFLoader } from 'https://threejsfundamentals.org/threejs/resources/threejs/r127/examples/jsm/loaders/GLTFLoader.js'
 
-let camera, scene, renderer, stars, starGeo, c_ring, l_ring, r_ring, mouse, raycaster
+let camera, scene, renderer, stars, starGeo, c_ring, l_ring, r_ring, mouse, raycaster, leftTube, centerTube, rightTube
+const clock = new THREE.Clock()
 let clicked = false
 let complete = false
+let cameraMove = false
+let selectedRing = ''
 const STAR_COUNT = 9000
 
 init()
@@ -15,12 +18,13 @@ function init () {
   camera.position.set(0, 1, 0)
   // camera.position.y = 2
 
+  // raycaster
   mouse = new THREE.Vector2()
   raycaster = new THREE.Raycaster()
 
   scene = new THREE.Scene()
   scene.background = new THREE.Color(0x141414)
-  // scene.fog = new THREE.Fog(0x00ff00, 0, 750)
+  // scene.fog = new THREE.Fog(0x141414, 100, 500)
 
   renderer = new THREE.WebGLRenderer({ antialias: true })
   renderer.setSize(window.innerWidth, window.innerHeight)
@@ -47,6 +51,56 @@ function init () {
   scene.add(light.target)
   scene.add(s_light.target)
 
+  // Adding curve paths for camera paths
+  // LEFT
+  const leftCurvePath = new THREE.CubicBezierCurve3(
+    new THREE.Vector3(0, 1, 0),
+    new THREE.Vector3(-15, 0, -8),
+    new THREE.Vector3(-8, -1, -15),
+    new THREE.Vector3(-9, -1, -16)
+  )
+
+  const lCurveGeo = new THREE.TubeBufferGeometry(leftCurvePath, 50, 0.01, 20, false)
+  const curveMaterial = new THREE.MeshBasicMaterial({ color: 0xffff00, wireframe: true })
+
+  leftTube = new THREE.Mesh(lCurveGeo, curveMaterial)
+  console.log('Added left line to scene', leftTube)
+
+  leftTube.visible = false
+  scene.add(leftTube)
+
+  // CENTER
+  const centerCurvePath = new THREE.CubicBezierCurve3(
+    new THREE.Vector3(0, 1, 0),
+    new THREE.Vector3(0, 1, -5),
+    new THREE.Vector3(0, 0, -12),
+    new THREE.Vector3(0, 0, -12)
+  )
+
+  const cCurveGeo = new THREE.TubeBufferGeometry(centerCurvePath, 50, 0.01, 20, false)
+  const cCurveMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true })
+
+  centerTube = new THREE.Mesh(cCurveGeo, cCurveMaterial)
+  centerTube.visible = false
+  console.log('Added center line to scene', centerTube)
+  scene.add(centerTube)
+
+  // RIGHT
+  const rightCurvePath = new THREE.CubicBezierCurve3(
+    new THREE.Vector3(0, 1, 0),
+    new THREE.Vector3(3, 1, -4),
+    new THREE.Vector3(8, -1, -8),
+    new THREE.Vector3(10, -1, -16)
+  )
+
+  const rCurveGeo = new THREE.TubeBufferGeometry(rightCurvePath, 50, 0.01, 20, false)
+  const rCurveMaterial = new THREE.MeshBasicMaterial({ color: 0x0000ff, wireframe: true })
+
+  rightTube = new THREE.Mesh(rCurveGeo, rCurveMaterial)
+  rightTube.visible = false
+  console.log('Added right line to scene', rightTube)
+  scene.add(rightTube)
+
   // Initialize Stars, setting Velocity and Acceleration
   starGeo = createStars(2, 0)
   loadModel()
@@ -69,7 +123,7 @@ function init () {
 
   // Left Ring
   loader.load('./assets/models/ring.glb', function (model) {
-    console.log('Added model to scene.', model.scene.children[2])
+    console.log('Added left model to scene.', model.scene.children[2])
     l_ring = model.scene.children[2]
     // l_ring.children[1].position.setXYZ(0, 1, 0)
     l_ring.rotateZ(Math.PI / 2)
@@ -84,7 +138,7 @@ function init () {
 
   // Right Ring
   loader.load('./assets/models/ring.glb', function (model) {
-    console.log('Added model to scene.', model.scene.children[2])
+    console.log('Added right model to scene.', model.scene.children[2])
     r_ring = model.scene.children[2]
     r_ring.rotateX(0)
     r_ring.rotateZ(Math.PI / 2)
@@ -124,24 +178,31 @@ function animation () {
   starSpeed(0.1)
   if (c_ring !== undefined) {
     // c_ring.rotateZ(0.0002)
+    c_ring.rotateX(0.0005)
     c_ring.rotateY(0.005)
     // c_ring.rotateX(-0.0002)
   }
 
   if (l_ring !== undefined) {
-    l_ring.rotateX(-0.002)
-    l_ring.rotateY(0.006)
+    l_ring.rotateX(-0.0005)
+    l_ring.rotateY(0.005)
+    l_ring.rotateZ(0.0001)
   }
 
   if (r_ring !== undefined) {
     r_ring.rotateY(-0.005)
-    r_ring.rotateX(0.003)
+    r_ring.rotateX(0.0005)
+    r_ring.rotateZ(0.0003)
   }
 
   if (clicked) {
     if (changeOpacity(0.005, c_ring) && changeOpacity(0.005, l_ring) && changeOpacity(0.005, r_ring)) {
       complete = true
     }
+  }
+
+  if (clicked && cameraMove) {
+    updateCamera(selectedRing)
   }
 
   resetRings()
@@ -223,7 +284,7 @@ function hoverRing () {
   const intersects = raycaster.intersectObjects(scene.children)
   for (let i = 0; i < intersects.length; i++) {
     if (!(intersects[i].object instanceof THREE.Points) && complete) {
-      console.log('object intersected', intersects)
+      // console.log('object intersected', intersects)
       intersects[i].object.material.transparent = true
       intersects[i].object.material.opacity = 0.1
     }
@@ -244,6 +305,10 @@ function resetRings () {
     if (r_ring.material.opacity <= 1) {
       changeOpacity(0.01, r_ring)
     }
+
+    if (leftTube.material.opacity <= 1) {
+      changeOpacity(0.01, leftTube)
+    }
   }
 }
 
@@ -262,7 +327,16 @@ function onWindowClick (event) {
   if (intersects.length > 0) {
     for (let i = 0; i < intersects.length; i++) {
       if (!(intersects[i].object instanceof THREE.Points)) {
-        console.log('Clicked the object!', intersects[i].object)
+        if (intersects[i].object === l_ring) {
+          cameraMove = true
+          selectedRing = 'left'
+        } else if (intersects[i].object === c_ring) {
+          cameraMove = true
+          selectedRing = 'center'
+        } else if (intersects[i].object === r_ring) {
+          cameraMove = true
+          selectedRing = 'right'
+        }
       } else {
         changeAcceleration(STAR_COUNT, -0.01)
         clicked = true
@@ -271,5 +345,35 @@ function onWindowClick (event) {
   } else {
     changeAcceleration(STAR_COUNT, -0.01)
     clicked = true
+  }
+}
+
+function updateCamera (ring) {
+  const time = clock.getElapsedTime()
+  const pathTime = 3
+  const t = (time % pathTime) / pathTime
+  const t2 = ((time + 0.1) % pathTime) / pathTime
+
+  if (t2 <= 0.99) {
+    if (ring === 'left') {
+      const pos = leftTube.geometry.parameters.path.getPointAt(t)
+      const pos2 = leftTube.geometry.parameters.path.getPointAt(t2)
+      camera.position.copy(pos)
+      camera.lookAt(pos2)
+    } else if (ring === 'center') {
+      const pos = centerTube.geometry.parameters.path.getPointAt(t)
+      const pos2 = centerTube.geometry.parameters.path.getPointAt(t2)
+      camera.position.copy(pos)
+      camera.lookAt(pos2)
+    } else if (ring === 'right') {
+      const pos = rightTube.geometry.parameters.path.getPointAt(t)
+      const pos2 = rightTube.geometry.parameters.path.getPointAt(t2)
+      camera.position.copy(pos)
+      camera.lookAt(pos2)
+    }
+  } else {
+    cameraMove = false
+    // changeAcceleration(STAR_COUNT, -0.001)
+    // starSpeed(0.005)
   }
 }
